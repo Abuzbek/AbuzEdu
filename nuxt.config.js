d42cb536201baad673d6e7c0eddcf797f0fbd04e
AbuzCoder <abuzcoder@gmail.com>
2020-11-28 08:40:25 +0500

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@/plugins/vuesax'
+    '@/plugins/vuesax',
+    '@/assets/css/all.min.css'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,155 @@
 <template>
-  <div class="container">
-    <div class="overlay">
-      <br>
-    </div>
-    <h1>
-      Welcome to AbuzEducation
-    </h1>
+  <div class="hidden">
+    <vs-navbar square center-collapsed v-model="active">
+      <template #left>
+        <vs-button @click="activeSidebar = !activeSidebar" flat icon>
+          <i class="far fa-bars"></i>
+        </vs-button>
+      </template>
+    </vs-navbar>
+    <vs-sidebar
+      absolute
+      v-model="active"
+      :open.sync="activeSidebar"
+    >
+      <template #logo>
+        <img :src="logo" alt="">
+      </template>
+      <vs-sidebar-item id="home">
+        <template #icon>
+          <i class="far fa-home"></i>
+        </template>
+        Home
+      </vs-sidebar-item>
+      <vs-sidebar-item id="market">
+        <template #icon>
+          <i class="fas fa-th-large"></i>
+        </template>
+        Market Overview
+      </vs-sidebar-item>
+      <vs-sidebar-item id="Music">
+        <template #icon>
+          <i class="far fa-music"></i>
+        </template>
+        Music
+      </vs-sidebar-item>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="far fa-user-friends"></i>
+            </template>
+            Social media
+          </vs-sidebar-item>
+        </template>
+        <vs-sidebar-item id="Instagram">
+          <template #icon>
+            <i class="fab fa-instagram"></i>
+          </template>
+          Instagram
+        </vs-sidebar-item>
+        <vs-sidebar-item id="twitter">
+          <template #icon>
+            <i class="fab fa-twitter"></i>
+          </template>
+          Twitter
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Facebook">
+          <template #icon>
+            <i class="fab fa-facebook"></i>
+          </template>
+          Facebook
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="far fa-code"></i>
+            </template>
+            Coding
+          </vs-sidebar-item>
+        </template>
+        <vs-sidebar-item id="github">
+          <template #icon>
+            <i class="fab fa-github"></i>
+          </template>
+          Github
+        </vs-sidebar-item>
+        <vs-sidebar-item id="codepen">
+          <template #icon>
+            <i class="fab fa-codepen"></i>
+          </template>
+          Codepen
+        </vs-sidebar-item>
+        <vs-sidebar-item id="discord">
+          <template #icon>
+            <i class="fab fa-discord"></i>
+          </template>
+          Discord
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Javascript">
+          <template #icon>
+            <i class="fab fa-js-square"></i>
+          </template>
+          Javascript
+        </vs-sidebar-item>
+        <vs-sidebar-item id="git">
+          <template #icon>
+            <i class="fab fa-git"></i>
+          </template>
+          Git
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <vs-sidebar-item id="donate">
+        <template #icon>
+          <i class="fas fa-hand-holding-heart"></i>
+        </template>
+        Donate
+      </vs-sidebar-item>
+      <vs-sidebar-item id="drink">
+        <template #icon>
+          <i class="far fa-glass-martini-alt"></i>
+        </template>
+        Drink
+      </vs-sidebar-item>
+      <vs-sidebar-item id="shopping">
+        <template #icon>
+          <i class="far fa-shopping-bag"></i>
+        </template>
+        Shopping
+      </vs-sidebar-item>
+      <vs-sidebar-item id="chat">
+        <template #icon>
+          <i class="far fa-comments-alt"></i>
+        </template>
+        Chat
+      </vs-sidebar-item>
+      <template #footer>
+        <vs-row justify="space-between">
+          <vs-avatar>
+            <img src="/avatars/avatar-5.png" alt="">
+          </vs-avatar>
+          <vs-avatar badge-color="danger" badge-position="top-right">
+            <i class="far fa-bell"></i>
+            <template #badge>
+              28
+            </template>
+          </vs-avatar>
+        </vs-row>
+      </template>
+    </vs-sidebar>
   </div>
 </template>
-
 <script>
-
+import logo from '~/assets/images/logo_03.png'
 export default {
-  components: {
+  data () {
+    return {
+      active: 'home',
+      activeSidebar: false,
+      logo
+    }
   }
 }
 </script>
-
-<style>
-  .container{
-    background:url('https://www.itl.cat/pngfile/big/291-2919409_developer-wallpaper.png') no-repeat center center  / cover;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-  }
-  .container h1{
-    font-size: 50px;
-    color: white;
-    font-family: sans-serif;
-    position: relative;
-  }
-  .container .overlay{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    backdrop-filter: blur(5px);
-    background: rgba(0, 0, 0, 0.5);
-  }
-</style>
